e4322bb1958c785007e34316c82e37cd6b3837be
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-06-06 20:10:21 +0800

    Auto-commit: Project implementation in progress [count: 1029]

---
+++ src/types/qrcode.react.d.ts
@@ -0,0 +1,17 @@
+declare module 'qrcode.react' {
+  import { ComponentType, CSSProperties } from 'react';
+
+  export interface QRCodeProps {
+    value: string;
+    size?: number;
+    level?: 'L' | 'M' | 'Q' | 'H';
+    bgColor?: string;
+    fgColor?: string;
+    includeMargin?: boolean;
+    style?: CSSProperties;
+  }
+
+  const QRCode: ComponentType<QRCodeProps>;
+  export default QRCode;
+  export { QRCode };
+}
