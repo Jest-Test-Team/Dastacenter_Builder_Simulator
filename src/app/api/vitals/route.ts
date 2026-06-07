71b47a0f003e8c4d716f24faabde862ea184e497
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-06-07 11:50:16 +0800

    update routes

--- src/app/api/vitals/route.ts
+++ src/app/api/vitals/route.ts
@@ -8,8 +8,6 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 
-export const runtime = 'edge';
-
 export async function POST(req: NextRequest) {
   try {
     const body = await req.text();
